a7f0aa24a4a86b18dcc0de7dc827367824bdcd03
ganben <caozhao@gmail.com>
2016-10-09 20:55:35 +0800

    js module, python example

---
+++ index.js
@@ -0,0 +1,9 @@
+//this module is to render my own map objects on amap canvas.
+
+module.exports = {}
+//export named functions;
+var MmText = function () {};
+
+MmText.prototype.log = function () {
+  console.log('micromap init');
+};
