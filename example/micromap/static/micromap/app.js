ee2f311a00ae737200971d3a5378d2b831d3ea26
ganben <caozhao@gmail.com>
2016-10-12 20:39:59 +0800

    fix django templ engine then js programming

--- example/micromap/static/micromap/app.js
+++ example/micromap/static/micromap/app.js
@@ -3,6 +3,47 @@ AMap.event.addListener(map, 'zoomend', function(){
 });
 
 var clickEventListener = map.on('click', function(e){
-  document.getElementById("lnglat").value = e.lnglat.getLng()+', '+ e.lnglat.getLat);
-  
+  document.getElementById("lnglat").value = e.lnglat.getLng()+', '+ e.lnglat.getLat();  
 });
+
+//{% for obj in objects %}
+//first loop
+objArray.forEach(function (obj) {
+switch (obj.type) {
+
+  case 'Marker':
+    
+    var marker = new AMap.Marker({
+      position: [obj.lnglat[0],obj.lnglat[1]]
+    });
+    marker.setMap(map);
+    marker.setLabel({
+      offset: new AMap.Pixel(20, 20),
+      content: obj.text
+    });
+    break;
+
+  case 'Polygon':
+    var polygonArr = new Array();
+    //{% for point in obj.path %}
+    obj.path.forEach(function (point){
+    //second loop
+    polygonArr.push(point);
+    })
+    //{% endfor %}
+    var polygon = new AMap.Polygon({
+      path: polygonArr,
+      strokeColor: "#FF33FF",
+      strokeWeight: 4,
+      fillColor: "#1791fc",
+    });
+    polygon.setMap(map);
+    break;
+
+
+  default:
+    break;
+
+}
+});
+//{% endfor %}
--- example/micromap/views.py
+++ example/micromap/views.py
@@ -9,6 +9,18 @@ from django.shortcuts import render
 amapKey = 'f7ac40ee5f12833a51b9ef058600b330'
 
 def index(request):
-	objects = []
-	#here objects append some, text, icon, polyline, polygon, etc
-	return render(request, 'micromap/index.html', {'amapKey': amapKey })
+    objects = [ ]
+    marker1 = {'type': 'Marker',
+            'lnglat':[ 113.962737,22.528965 ],
+            'text': 'good marker'}
+    polygon1 = {'type': 'Polygon',
+            'lnglat':[ 113.955173,22.532146 ],
+            'path': [[ 113.95631,22.531987 ],
+                [ 113.95601,22.53058 ],
+                [ 113.95586,22.528618 ],
+                [ 113.954744,22.528935 ]]}
+    objects.append(marker1)
+    # objects.append(polygon1)
+    #here objects append some, text, icon, polyline, polygon, etc
+    return render(request, 'micromap/index.html', {'amapKey': amapKey,
+        'objects':objects})
