6966e1c1c476a176b2ed64a08cc38c06b621206c
ganben <caozhao@gmail.com>
2016-10-13 20:17:37 +0800

    complete 1st obj

--- example/micromap/views.py
+++ example/micromap/views.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,10 +10,12 @@ from django.shortcuts import render
 amapKey = 'f7ac40ee5f12833a51b9ef058600b330'
 
 def index(request):
-    objects = [ ]
+    objects = [ ]# this is a example of 
+    #one map obj included array of drawable objects, which is defined by AMap
+    #APIs;
     marker1 = {'type': 'Marker',
-            'lnglat':[ 113.962737,22.528965 ],
-            'text': 'good marker'}
+            'lnglat':[ 113.956112, 22.530516 ],
+            'text': '大沙河停车场（11/19）'}
     polygon1 = {'type': 'Polygon',
             'lnglat':[ 113.955173,22.532146 ],
             'path': [[ 113.95631,22.531987 ],
@@ -20,7 +23,7 @@ def index(request):
                 [ 113.95586,22.528618 ],
                 [ 113.954744,22.528935 ]]}
     objects.append(marker1)
-    # objects.append(polygon1)
+    objects.append(polygon1)
     #here objects append some, text, icon, polyline, polygon, etc
     return render(request, 'micromap/index.html', {'amapKey': amapKey,
         'objects':objects})
