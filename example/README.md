8e43d869fdf746de7ca63dc73d27b858887093c7
ganben <caozhao@gmail.com>
2016-10-19 20:56:06 +0800

    add mongoengine and db models

---
+++ example/README.md
@@ -0,0 +1,10 @@
+# Example projects
+#Server: Python-Django-Dependencies
+
+python 2.7.11+
+django 1.10
+mongoengine
+
+#Front-End: vuejs dependencies
+
+
--- example/micromap/models.py
+++ example/micromap/models.py
@@ -1,5 +1,30 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from mongoengine import *
+import datetime
 # Create your models here.
+
+connect('test')
+
+class Organization(Document):
+    title = StringField(max_length=50, required=True)
+    date_modified = DateTimeField(default=datetime.datetime.now)
+    level = IntField(default=0) # represent levels, 0 is root level
+    parent = ReferenceField('self') #self linked, has only one parent
+
+
+class MmapObj(Document):
+    lnglat = GeoPointField(required=True)
+    name = StringField(max_length=100, required=True)
+    owner = ReferenceField(Organization)  #linked to organization model
+    visible = BooleanField(default=True)
+    address = StringField(max_length=100)
+    level = IntField(default=0)  #root level = 0
+    parent = ReferenceField('self') #self netted relationships
+    draw = ListField(ReferenceField(Drawable))  #One-To-Many relationships
+
+class Drawable(DynamicDocument):
+    type = StringField(required=True, max_length=20)
+    lnglat = GeoPointField(required=True)
+
